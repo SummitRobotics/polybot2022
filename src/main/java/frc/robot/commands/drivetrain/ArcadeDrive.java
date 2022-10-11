3ebcf36a1b12c44d4357a6c144d5cc86b64f12cd
Owen McCormick <oweninoregon@gmail.com>
2022-10-11 16:28:53 -0700

    Wrote Drivetrain.java and ArcadeDrive.java

---
+++ src/main/java/frc/robot/commands/drivetrain/ArcadeDrive.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.drivetrain;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class ArcadeDrive extends CommandBase {
+
+    private Joystick joystick;
+    private DifferentialDrive robotDrive;
+
+  /**
+   * Constructor for ArcadeDrive
+   *
+   * @param drivetrain The drivetrain subsystem
+   * @param joystick The joystick controlling the drivetrain
+   */
+  public ArcadeDrive(Drivetrain drivetrain, Joystick joystick) {
+
+    this.joystick = joystick;
+    robotDrive = new DifferentialDrive(drivetrain.left, drivetrain.right);
+    addRequirements(drivetrain);
+  }
+
+// Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    robotDrive.arcadeDrive(-joystick.getY(), joystick.getX());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.lists.Ports;
+
+public class Drivetrain extends SubsystemBase{
+    public final CANSparkMax left = new CANSparkMax(Ports.LEFT_DRIVE, MotorType.kBrushless);
+    public final CANSparkMax right = new CANSparkMax(Ports.RIGHT_DRIVE, MotorType.kBrushless);
+
+    public Drivetrain() {
+        right.setInverted(true);
+    }
+}
